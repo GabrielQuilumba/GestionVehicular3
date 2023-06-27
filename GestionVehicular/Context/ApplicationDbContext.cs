f0d2c4eefea82754192788a9f8141cf11f1fe841
GabrielQuilumba <137961983+GabrielQuilumba@users.noreply.github.com>
2023-06-27 18:54:23 -0500

    task: add context

---
+++ GestionVehicular/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionVehicular.Models;
+
+namespace GestionVehiculos.Context;
+
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    public DbSet<Distrito> Distritos { get; set; }
+
+    public DbSet<Circuito> Circuitos { get; set; }
+
+    public DbSet<Subcircuito> Subcircuito { get; set; }
+
+    public DbSet<GestionVehicular.Models.Vehiculo> Vehiculo { get; set; }
+
+    public DbSet<GestionVehicular.Models.TipoMantenimiento> TipoMantenimiento { get; set; }
+
+}
